3dd1e6d8148863895792b971377f4b90ad1641be
youngbloodcyb <cameron.youngblood@gmail.com>
2023-01-31 21:56:55 -0700

    update transition time

--- pic-scroll/index.js
+++ pic-scroll/index.js
@@ -3,7 +3,6 @@ const imgs = document.querySelectorAll("img");
 imgs.forEach(img => {
     img.addEventListener("mouseover", (e) => {
         e.target.width = 400;
-        // e.target.style.transitionTimingFunction = "cubic-bezier(.17,.67,.53,.92)"
     })
     img.addEventListener("mouseout", (e) => {
         e.target.width = 100;
