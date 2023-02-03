8a2074d5cc514357b2988c9e2fcae3b5d7da5fe7
youngbloodcyb <95453018+youngbloodcyb@users.noreply.github.com>
2023-02-03 10:52:50 -0700

    add index to for each loop

--- hidden-components/index.js
+++ hidden-components/index.js
@@ -1,9 +1,10 @@
 console.log("test")
 
 const elems = document.querySelectorAll('button');
-elems.forEach((el) => {
+elems.forEach((el, index) => {
     el.addEventListener("click", (e) => {
         el.dataset.visible = "true";
         console.log("test")
     })
 })
+// need to change to select the hidden div and change data attribute using index
